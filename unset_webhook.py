766cc73e4ef9955acc11199aac84e7705fb03aba
Shamanche <72706516+Shamanche@users.noreply.github.com>
2021-11-06 03:40:43 +0300

    Success deploy

---
+++ unset_webhook.py
@@ -0,0 +1,17 @@
+
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from os import environ
+from dotenv import load_dotenv
+load_dotenv()
+AUTH_TOKEN = environ.get('VIBER_API_KEY')
+
+# сюда нужно вставить инфу со своего бота
+viber = Api(BotConfiguration(
+    name='PythonSampleBot',
+    avatar='',
+    auth_token=AUTH_TOKEN
+))
+viber.unset_webhook()
+
+print('Done.')
